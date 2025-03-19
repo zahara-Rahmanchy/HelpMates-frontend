3d5f4ae4d2203e66d88d62b4751c8210265489c1
zahara-Rahmanchy <zaharachy72@gmail.com>
2025-03-19 17:21:47 +0600

    Updated Dashboard page

--- src/app/(dashboardLayout)/Dashboard/page.tsx
+++ src/app/(dashboardLayout)/Dashboard/page.tsx
@@ -1,9 +1,4 @@
 "use client";
-import {BarChartComponent} from "@/components/UI/Dashboard/MetaData/BarChartComponent";
-import PieChartComponent from "@/components/UI/Dashboard/MetaData/PieChartComponent";
-import TopCards from "@/components/UI/Dashboard/MetaData/TopCards";
-import UserGrowthLine from "@/components/UI/Dashboard/MetaData/SignupsDashboard";
-import {useGetDashboardDataQuery} from "@/redux/api/DashboardMetaApi";
 import {
   Backdrop,
   Box,
@@ -18,62 +13,17 @@ import SignupsDashboard from "@/components/UI/Dashboard/MetaData/SignupsDashboar
 import {getUserInfo} from "@/services/auth.services";
 
 const DashboardComp = () => {
-  const {data: dashboardData, isLoading} = useGetDashboardDataQuery("");
-  console.log("dashboard: ", dashboardData);
-
-  const {role} = getUserInfo() as any;
-  console.log("role: ", role);
-  console.log("userinfr: ", getUserInfo());
-  // const countData = dashboardData?.counts;
   return (
     <Box margin={"0 auto"} width="100%">
-      {/* <Typography
+      <Typography
         height={"100%"}
         textAlign={"center"}
         variant="h6"
         fontSize={"20px"}
         mt={2}
       >
-        Welcome to Admin Dashbaord
-      </Typography> */}
-      {isLoading && (
-        <Backdrop
-          sx={{color: "#fff", zIndex: theme => theme.zIndex.drawer + 1}}
-          open={isLoading}
-        >
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      )}
-      {!isLoading && (
-        <>
-          {dashboardData?.counts !== undefined && (
-            <TopCards countData={dashboardData?.counts} />
-          )}
-          <Grid container spacing={2} my={8}>
-            <Grid item xs={12} lg={6} sx={{gridRow: "span 2"}}>
-              {dashboardData?.opportunityPieDataWithPercentages !==
-                undefined && (
-                <PieChartComponent
-                  piechartData={dashboardData.opportunityPieDataWithPercentages}
-                />
-              )}
-            </Grid>
-            <Grid item xs={12} lg={6}>
-              {dashboardData?.applicationStatusBarChart !== undefined && (
-                <BarChartComponent
-                  barchartData={dashboardData?.applicationStatusBarChart}
-                />
-              )}
-            </Grid>
-            <Grid item xs={12}>
-              {role === "Admin" && <SignupsDashboard />}
-              {/* {dashboardData?.applicationStatusBarChart !== undefined && (
-                <UserGrowthLine data={dashboardData?.userGrowthData} />
-              )} */}
-            </Grid>
-          </Grid>
-        </>
-      )}
+        Welcome to Dashbaord
+      </Typography>
     </Box>
   );
 };
