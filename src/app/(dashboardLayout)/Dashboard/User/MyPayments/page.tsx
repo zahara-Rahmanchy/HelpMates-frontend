20aa932912b1833393be5721357345df3f31a62d
zahara-Rahmanchy <zaharachy72@gmail.com>
2025-10-14 18:23:15 +0600

    mypayments page added

---
+++ src/app/(dashboardLayout)/Dashboard/User/MyPayments/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyPayments = () => {
+  return (
+    <div  className='w-full mx-auto'>
+       <h3>Coming Soon!</h3>
+    </div>
+  )
+}
+
+export default MyPayments
